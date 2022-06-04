078d3ca91aa1682d597380ede1b540512aeaa0aa
arapl3y <rapley3@gmail.com>
2022-06-04 22:07:26 +1000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    extend: {
+      colors: {
+        ffPink: "#f05969",
+      },
+    },
+  },
+  plugins: [],
+};
