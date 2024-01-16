1c0166a6ddfa8c974c3476f7040fb8cb2f806188
Alex Rapley <rapley3@gmail.com>
2024-01-16 22:27:56 +1100

    Add under construction

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&display=swap"
+        rel="preload"
+        as="style"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,81 +1,137 @@
-import type { NextPage } from "next";
+import { useEffect, useRef, useState } from "react";
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import type { NextPage } from "next";
+import gsap from "gsap";
+import SplitType from "split-type";
+
+const links = [
+  {
+    href: "//github.com/arapl3y",
+    text: "Github ↗",
+  },
+  {
+    href: "//www.linkedin.com/in/alex-rapley-7a00b159/",
+    text: "LinkedIn ↗",
+  },
+  {
+    href: "mailto:rapley3@gmail.com",
+    text: "Email ↗",
+  },
+];
 
 const Home: NextPage = () => {
-  const [currentFrame, setCurrentFrame] = useState<number>(0);
+  const [splitText, setSplitText] = useState<SplitType>();
+  const linksRef = useRef<HTMLAnchorElement[]>([]);
 
-  const asciiOwlFrames = [
-    `
-/\\_/\\
-((-v-))
-():::()
-/---m-m---/
-  `,
-    `
-/\\_/\\
-((-v-))
-():::()
-/---m-m---/
-  `,
-    `
-/\\_/\\
-((ov-))
-():::()
-/---m-m---/
-    `,
-    `
-/\\_/\\
-((ovo))
-():::()
-/---m-m---/
-    `,
-    `
-/\\_/\\
-((^v^))
-():::()
-/---m-m---/
-    `,
-    `
-/\\_/\\
-((^v^))
-():::()
-/---m-m---/
-    `,
-  ];
-
+  // Title animation
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentFrame((prev) => (prev += 1) % asciiOwlFrames.length);
-    }, 1250);
+    const splitTitle = new SplitType(".title", {
+      types: "chars",
+    });
 
-    return () => clearInterval(interval);
-  }, [asciiOwlFrames.length]);
+    gsap.to(splitTitle.chars, {
+      yPercent: -100,
+      duration: 0.8,
+      ease: "power4.inOut",
+      stagger: { each: 0.02 },
+      onComplete() {
+        gsap.set(splitTitle.chars, {
+          yPercent: 0,
+        });
+      },
+    });
+
+    return () => {
+      splitTitle.revert();
+    };
+  }, []);
+
+  // Split text for link animation
+  useEffect(() => {
+    const splitLinks = new SplitType(".menu-link", {
+      types: "chars",
+    });
+
+    setSplitText(splitLinks);
+
+    return () => {
+      splitLinks.revert();
+    };
+  }, []);
+
+  // Resize
+  useEffect(() => {
+    function handleResize() {
+      if (!splitText) return;
+
+      splitText.revert();
+
+      const splitLinks = new SplitType(".menu-link", {
+        types: "chars,words",
+      });
+
+      setSplitText(splitLinks);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
+  // Handlers for animation
+  const handleMouseEnter = (index: number) => {
+    const chars = linksRef.current[index].querySelectorAll(".char");
+
+    gsap.to(chars, {
+      yPercent: -80,
+      duration: 0.5,
+      ease: "power4.inOut",
+      stagger: { each: 0.01 },
+      overwrite: true,
+    });
+  };
+
+  const handleMouseLeave = (index: number) => {
+    const chars = linksRef.current[index].querySelectorAll(".char");
+
+    gsap.to(chars, {
+      yPercent: 0,
+      duration: 0.4,
+      ease: "power4.inOut",
+      stagger: { each: 0.01 },
+    });
+  };
 
   return (
     <div className="container h-screen flex justify-center items-center">
       <Head>
-        <title>Alex Rapley</title>
+        <title>🚧 🚧 🚧</title>
         <meta name="description" content="Alex Rapley's personal site" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <div className="flex flex-col items-center justify-center text-center max-w-md">
-          <pre>{asciiOwlFrames[currentFrame]}</pre>
+        <div className="flex flex-col items-center justify-center text-center">
+          <h1 className="title text-3xl md:text-6xl xl:text-8xl font-bold uppercase overflow-hidden whitespace-nowrap">
+            Under construction
+          </h1>
 
-          <ul className="flex gap-2 font-body text-blue underline text-xs">
-            <li>
-              <a href="//github.com/arapl3y">Github</a>
-            </li>
-
-            <li>
-              <a href="//www.linkedin.com/in/alex-rapley-7a00b159/">LinkedIn</a>
-            </li>
-
-            <li>
-              <a href="mailto:rapley3@gmail.com">Email</a>
-            </li>
+          <ul className="mt-20 flex gap-8 font-body uppercase text-md md:text-2xl font-bold overflow-hidden">
+            {links.map((link, index) => (
+              <li key={index}>
+                <a
+                  href={link.href}
+                  className="menu-link stagger-link"
+                  onMouseEnter={() => handleMouseEnter(index)}
+                  onMouseLeave={() => handleMouseLeave(index)}
+                  ref={(el) => el && linksRef.current?.push(el)}
+                >
+                  <span className="menu-link-text">{link.text}</span>
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </main>
