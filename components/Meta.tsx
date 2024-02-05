7acc48fa1e5e1c12311315a4a7c90fc8a38f166a
Alex Rapley <rapley3@gmail.com>
2024-02-05 15:53:27 +1100

    Fix twitter:card

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -4,31 +4,27 @@ const Meta = ({
   title = "Alex Rapley",
   description = "Alex Rapley's personal site",
   image = "/meta.png",
-}) => {
-  console.log(description);
+}) => (
+  <Head>
+    <title>{title}</title>
+    <meta name="title" content={title} />
+    <meta name="description" content={description} />
+    <link rel="icon" href="/favicon.ico" />
 
-  return (
-    <Head>
-      <title>{title}</title>
-      <meta name="title" content={title} />
-      <meta name="description" content={description} />
-      <link rel="icon" href="/favicon.ico" />
+    {/* <!-- Open Graph / Facebook --> */}
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://alexrapley.com/" />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta property="og:image" content={image} />
 
-      {/* <!-- Open Graph / Facebook --> */}
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://alexrapley.com/" />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
-
-      {/* <!-- Twitter --> */}
-      <meta property="twitter:card" content={image} />
-      <meta property="twitter:url" content="https://alexrapley.com/" />
-      <meta property="twitter:title" content={title} />
-      <meta property="twitter:description" content={description} />
-      <meta property="twitter:image" content={image} />
-    </Head>
-  );
-};
+    {/* <!-- Twitter --> */}
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://alexrapley.com/" />
+    <meta property="twitter:title" content={title} />
+    <meta property="twitter:description" content={description} />
+    <meta property="twitter:image" content={image} />
+  </Head>
+);
 
 export default Meta;
