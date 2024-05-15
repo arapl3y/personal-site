7378a7d5ff017ff7108969a6a17157152314330b
Alex Rapley <rapley3@gmail.com>
2024-05-15 17:34:46 +1000

    Read from sanity config in client

--- sanity/client.ts
+++ sanity/client.ts
@@ -1,10 +1,9 @@
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import config from "@/sanity.config";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? "";
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET ?? "";
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION ?? "";
+const { projectId, dataset, apiVersion } = config;
 
 const client = createClient({
   projectId,
