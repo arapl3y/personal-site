8cb39272a69251ce604dd92c714faff50b969d68
Alex Rapley <rapley3@gmail.com>
2024-08-01 12:14:09 +1000

    Update copy

--- components/Info.tsx
+++ components/Info.tsx
@@ -2,26 +2,22 @@ const Info = () => {
   return (
     <section className="text-off-black dark:text-off-white mt-12 md:mt-24 flex w-full max-w-4xl flex-col gap-4">
       <p>
-        Lead Product Engineer at{" "}
-        <a href="https://futurefriendly.team" className="underline">
-          Future Friendly
-        </a>
-        , based on Gadigal land in Australia — developing products that improve
-        people&apos;s everyday lives.
+        Software Engineer based on Gadigal land in Australia — developing
+        products that improve people&apos;s everyday lives.
       </p>
 
       <div>
         <p>
-          Working on large, small, production and prototype projects for
-          organisations across Government, Education, Finance, Environment and
-          Health.
+          Experience working on large, small, production and prototype projects
+          for organisations across Government, Education, Finance, Environment
+          and Health.
         </p>
 
         <p className="my-4">Specialising in:</p>
         <ul className="ml-2 flex flex-col">
-          <li>⭢ Full-stack development</li>
-          <li>⭢ Creative technology</li>
-          <li>⭢ Motion design</li>
+          <li>⭢ Development</li>
+          <li>⭢ Design</li>
+          <li>⭢ Motion</li>
         </ul>
       </div>
     </section>
